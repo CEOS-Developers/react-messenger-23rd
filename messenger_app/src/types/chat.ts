20a9adae796b1d3006dd82c43d5674ad040787f7
YJ <hghk0046@gmail.com>
2026-03-24 16:59:26 +0900

    feat: 유저 및 메시지 타입 정의

--- messenger_app/src/types/chat.ts
+++ messenger_app/src/types/chat.ts
@@ -5,8 +5,10 @@ export interface User {
 }
 
 export interface Message {
+  // 현재 내가 보고 있는 id와 senderId가 일치하면 내 시점으로, 그렇지 않으면 상대가 보낸 메시지로 보이게
   id: string;
   senderId: string;
   text: string;
+  isRead: boolean;
   timestamp: string;
 }
