5c14daab15a86cd5e6e63ee17a36d8fee53565ea
YuminPark <p353700@gmail.com>
2026-03-24 22:47:37 +0900

    [CHORE] 스크롤 영역 수정

--- src/layout/PublicLayout.tsx
+++ src/layout/PublicLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 
-import StatusBar from "@/assets/images/StatusBar.svg?react";
+// import StatusBar from "@/assets/images/StatusBar.svg?react";
 import Navibar from "@/components/Common/Navibar";
 
 const PublicLayout = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <main className="relative h-203 w-93.75 overflow-hidden bg-white shadow-md">
-        <StatusBar />
-        <Outlet />
+        {/* <StatusBar /> */}
+        <div className="flex-1 overflow-hidden">
+          <Outlet />
+        </div>
         <div className="absolute right-5 bottom-4 left-5">
           <Navibar />
         </div>
