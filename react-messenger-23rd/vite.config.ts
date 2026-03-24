5289a03e043b358cfa13aa9aa5b32bcd851cd3f2
a-00-a <sy.lee.2391@gmail.com>
2026-03-24 22:49:13 +0900

    Chore: tailwindcss 기본 설정

--- react-messenger-23rd/vite.config.ts
+++ react-messenger-23rd/vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -8,7 +9,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   envDir: '.env',
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [react(), svgr(), tsconfigPaths(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
