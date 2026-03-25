fb787e9bd10223f2d3d15ce1d69d0c379addc2a7
이윤서 <yyoo04@naver.com>
2026-03-26 00:15:47 +0900

    Mod: QA-상대방 변경 기능 삭제

--- messenger-whatsapp/src/components/chat/ChatHeader.tsx
+++ messenger-whatsapp/src/components/chat/ChatHeader.tsx
@@ -1,7 +1,6 @@
 import BackIcon from "../../assets/left.svg?react";
 import CameraIcon from "../../assets/camera.svg?react";
 import CallICon from "../../assets/call.svg?react";
-import { useChatStore } from "../../store/useChatStore";
 
 interface ChatHeaderProps {
   chatName: string;
@@ -9,15 +8,13 @@ interface ChatHeaderProps {
 }
 
 const ChatHeader = ({ chatName }: ChatHeaderProps) => {
-  const swapPerspective = useChatStore((s) => s.swapPerspective);
+  // const swapPerspective = useChatStore((s) => s.swapPerspective);
 
   return (
     <div className="flex justify-between px-4 py-2.5">
       <div className="flex flex-row items-center gap-3 typo-headline-2">
         <BackIcon className="cursor-pointer" />
-        <p className="cursor-pointer" onClick={swapPerspective}>
-          {chatName}
-        </p>
+        <p>{chatName}</p>
       </div>
       <div className="flex flex-row items-center gap-2">
         <CameraIcon />
