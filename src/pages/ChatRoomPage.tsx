2d4837f6ed12c891783e375a82a9237742c060d8
YuminPark <p353700@gmail.com>
2026-03-25 17:27:15 +0900

    [FEAT] messages.json에 date 필드 추가 및 날짜별 MessageDate 렌더링

--- src/pages/ChatRoomPage.tsx
+++ src/pages/ChatRoomPage.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import BackIcon from "@/assets/icons/back.svg?react";
 import CallIcon from "@/assets/icons/call.svg?react";
 import Message from "@/components/ChatRoom/Message";
@@ -32,27 +34,33 @@ const ChatRoomPage = () => {
       <Header leftIcon={<BackIcon />} text="김예지" rightIcon={<CallIcon />} scrolled={scrolled} />
       <div className="flex-1 overflow-y-auto" onScroll={handleScroll}>
         <div className="flex flex-col gap-5 pt-2">
-          <div className="flex justify-center">
-            <MessageDate date={new Date()} />
-          </div>
-          <div className="flex flex-col gap-8">
-            {groups.map((group, gi) => (
-              <div key={gi} className="flex flex-col gap-1">
-                {group.map((msg, mi) => (
-                  <Message
-                    key={mi}
-                    type={msg.type}
-                    name={msg.type === "friend" ? userJson.name : ""}
-                    message={msg.message}
-                    time={msg.time}
-                    isRead={msg.isRead}
-                    isFirst={mi === 0}
-                    showReadStatus={msg.showReadStatus}
-                  />
-                ))}
-              </div>
-            ))}
-          </div>
+          {groups.map((group, groupIndex) => {
+            const prevGroup = groups[groupIndex - 1];
+            const showDate = !prevGroup || prevGroup[0].date !== group[0].date;
+            return (
+              <React.Fragment key={groupIndex}>
+                {showDate && (
+                  <div className="flex justify-center">
+                    <MessageDate date={group[0].date} />
+                  </div>
+                )}
+                <div className="flex flex-col gap-1">
+                  {group.map((msg, msgIndex) => (
+                    <Message
+                      key={msgIndex}
+                      type={msg.type}
+                      name={msg.type === "friend" ? userJson.name : ""}
+                      message={msg.message}
+                      time={msg.time}
+                      isRead={msg.isRead}
+                      isFirst={msgIndex === 0}
+                      showReadStatus={msg.showReadStatus}
+                    />
+                  ))}
+                </div>
+              </React.Fragment>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/types/message.ts
+++ src/types/message.ts
@@ -3,6 +3,7 @@ export type MessageType = "my" | "friend";
 export interface MessageItem {
   type: MessageType;
   message: string;
+  date: string;
   time: string;
   isRead: boolean;
   showReadStatus: boolean;
