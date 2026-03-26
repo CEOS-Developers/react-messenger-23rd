b7cc0061b4233c4ccabc82d1e9749152ba24cecf
Mingyo Ku <mingo0273@gmail.com>
2026-03-27 03:08:13 +0900

    chore: trigger vercel redeploy

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -6,7 +6,7 @@ export default function MobileLayout({ children }: MobileLayoutProps) {
   return (
     <div className="flex min-h-screen items-start justify-center bg-[#EDEDEF] p-6">
       <div
-        className="overflow-hidden rounded-[16px] border border-[#DEDEDE] bg-white"
+        className="overflow-hidden rounded-[35px] border border-[#DEDEDE] bg-white"
         style={{
           width: "375px",
           height: "812px",
