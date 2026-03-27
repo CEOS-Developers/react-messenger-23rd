c73f0fc25df854f0d207a2ad85315a60f58bcc44
minseo <kimminseo4216@gmail.com>
2026-03-28 07:59:17 +0900

    [FEAT] 메시지 입력창 UI 구현

--- src/pages/ChatRoomPage.tsx
+++ src/pages/ChatRoomPage.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
+import MessageInput from '@/components/chatRoom/MessageInput'
 import ChatRoomHeader from '@/components/layout/ChatRoomHeader'
+import HomeIndicator from '@/components/common/HomeIndicator'
 import PageFrame from '@/components/layout/PageFrame'
 
 type ChatRoomPageProps = {
@@ -8,12 +12,24 @@ type ChatRoomPageProps = {
 }
 
 function ChatRoomPage({ chatName, memberCount, onBack }: ChatRoomPageProps) {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleSend = () => {
+    if (inputValue.trim()) {
+      setInputValue('')
+    }
+  }
+
   return (
     <PageFrame>
       <div className="flex h-full flex-col bg-white">
         <div className="h-[48px] shrink-0" />
         <ChatRoomHeader title={chatName} memberCount={memberCount} onBack={onBack} />
         <div className="flex-1 bg-white" />
+        <MessageInput value={inputValue} onChange={setInputValue} onSend={handleSend} />
+        <div className="flex h-[34px] items-center justify-center">
+          <HomeIndicator />
+        </div>
       </div>
     </PageFrame>
   )
