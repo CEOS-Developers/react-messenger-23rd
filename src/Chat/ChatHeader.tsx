c377d728bb8fe15d3ed8de873638589d129e82dc
girimNam <rlfla@naver.com>
2026-03-28 02:11:32 +0900

    chore: 채팅방 헤더 수정

--- src/Chat/ChatHeader.tsx
+++ src/Chat/ChatHeader.tsx
@@ -1,6 +1,7 @@
-import backArrow from '@assets/left.svg'
+//import backArrow from '@assets/left.svg'
 import hamBurger from '@assets/hamburg.svg'
 import searchIcon from '@assets/search.svg'
+import BackIcon from '@assets/leftArrow'
 
 interface Props {
   title: string
@@ -10,11 +11,8 @@ function ChatHeader({ title = '친구 이름' }: Props) {
   return (
     <div className="flex items-center justify-between py-2 px-3 h-14 bg-none border-b border-[0.75px] border-[var(--gray-30)]">
       <div className="flex items-center gap-1">
-        <button className="w-[36px] h-[36px] p-[6px] flex items-center justify-center">
-          <img
-            src={backArrow}
-            alt="뒤로가기"
-          />
+        <button className="w-[36px] h-[36px] p-[6px] flex items-center justify-center text-[var(--gray-90)]">
+          <BackIcon className="w-6 h-6" />
         </button>
 
         <div className="font-semibold overflow-hidden text-ellipsis whitespace-nowrap text-[17px] leading-[22px] tracking-[-0.17px] text-[var(--gray-95)]">
