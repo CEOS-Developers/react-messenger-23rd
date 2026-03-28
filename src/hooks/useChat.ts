c01d67ab3fc2845fe2280ff64a7ca31ce24d9a6e
girimNam <rlfla@naver.com>
2026-03-28 23:50:40 +0900

    chore: 기본 채팅 내용 추가

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -4,11 +4,39 @@ import type { Message } from '@/types/chat'
 //채팅 리스트, 메시지
 export function useChat() {
   const [chatList, setChatList] = useState<Message[]>([])
+  const dummyData: Message[] = [
+    {
+      id: '1',
+      text: '강의실 변경 확인해주세요.',
+      sender: 'other',
+      time: '15:14',
+      date: '2026-03-09',
+      unreadCount: 0,
+    },
+    {
+      id: '2',
+      text: '확인하였습니다.',
+      sender: 'me',
+      time: '15:14',
+      date: '2026-03-09',
+      unreadCount: 0,
+    },
+    {
+      id: '3',
+      text: '채팅방에 한하여 전체 프레임 좌우 마진을 12로 설정합니다.',
+      sender: 'me',
+      time: '15:15',
+      date: '2026-03-09',
+      unreadCount: 0,
+    },
+  ]
 
   useEffect(() => {
     const saved = localStorage.getItem('chatList')
     if (saved) {
       setChatList(JSON.parse(saved))
+    } else {
+      setChatList(dummyData)
     }
   }, [])
 
@@ -19,15 +47,19 @@ export function useChat() {
   const sendMessage = (text: string) => {
     if (!text.trim()) return
 
+    const now = new Date()
+
     const newMessage: Message = {
       id: crypto.randomUUID(),
       text,
       sender: 'me',
-      time: new Date().toLocaleTimeString([], {
+      date: now.toISOString().split('T')[0],
+      time: now.toLocaleTimeString([], {
         hour: '2-digit',
         minute: '2-digit',
         hour12: false,
       }),
+      unreadCount: 1,
     }
 
     setChatList((prev) => [...prev, newMessage])
