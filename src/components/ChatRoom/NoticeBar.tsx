3afd49765e11b41c82213dd7e07d2b239076a878
yujinnie <yujinnieohh@gmail.com>
2026-03-28 23:42:33 +0900

    feat: implement NoticeBar

--- src/components/ChatRoom/NoticeBar.tsx
+++ src/components/ChatRoom/NoticeBar.tsx
@@ -1,3 +1,23 @@
+import { IoIosArrowDown } from "react-icons/io";
+
 export default function NoticeBar() {
-  return <></>;
+  return (
+    <div className="px-3 py-2 w-full">
+      <div className="flex items-center justify-between bg-gray70 px-4 py-3 rounded-lg">
+        <div className="flex items-center gap-3">
+          <img
+            src="/images/icons/Flag.png"
+            alt="공지"
+            className="w-5 h-5 object-contain text-main2"
+          />
+
+          <span className="text-white font-body-01">공지</span>
+        </div>
+
+        <button className="text-white text-2xl">
+          <IoIosArrowDown />
+        </button>
+      </div>
+    </div>
+  );
 }
