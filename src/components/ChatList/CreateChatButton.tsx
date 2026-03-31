d7d321bc351d82d3b9940dcdf303e741d7eeada4
YuminPark <p353700@gmail.com>
2026-03-31 13:43:24 +0900

    [FEAT] 채팅 생성 버튼, 알람개수 컴포넌트 구현

---
+++ src/components/ChatList/CreateChatButton.tsx
@@ -0,0 +1,11 @@
+import AddChatIcon from "@/assets/icons/add_chat.svg?react";
+
+const CreateChatButton = () => {
+  return (
+    <div className="bg-gradation flex size-12.5 cursor-pointer items-center justify-center rounded-full">
+      <AddChatIcon />
+    </div>
+  );
+};
+
+export default CreateChatButton;
--- src/pages/ChatListPage.tsx
+++ src/pages/ChatListPage.tsx
@@ -1,4 +1,6 @@
 import ChatTime from "@/components/ChatList/ChatTime";
+import CreateChatButton from "@/components/ChatList/CreateChatButton";
+import Alert from "@/components/Common/Alert";
 import Header from "@/components/Common/Header";
 import useScrolled from "@/hooks/useScrolled";
 
@@ -9,6 +11,9 @@ const ChatListPage = () => {
     <div className="flex h-full flex-col">
       <Header text="대화" showShadow={scrolled} />
       <main className="flex-1 overflow-y-auto" onScroll={handleScroll}>
+        <Alert count={100} />
+        <Alert count={9} />
+        <CreateChatButton />
         <ChatTime time="오후 10:53" isRead={true} />
         <ChatTime time="오후 10:53" isRead={true} isFixed={true} />
         <ChatTime time="오후 10:53" isRead={false} />
