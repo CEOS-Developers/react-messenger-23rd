b63c768427888f3d3eddf43110a60eec69134ca1
YuminPark <p353700@gmail.com>
2026-04-01 15:27:16 +0900

    [CHORE] messageDate 컴포넌트 수정

--- src/components/ChatRoom/MessageDate.tsx
+++ src/components/ChatRoom/MessageDate.tsx
@@ -9,7 +9,7 @@ interface MessageDateProps {
 
 const MessageDate = ({ date, format = "short" }: MessageDateProps) => (
   <div className="rounded-100 inline-flex items-center justify-center bg-gray-200 px-2 py-0.5">
-    <span className="font-body-5 w-15.5 text-center text-gray-500">
+    <span className="font-caption-2 w-12 text-center text-gray-500">
       {formatDisplayDate(date, format)}
     </span>
   </div>
