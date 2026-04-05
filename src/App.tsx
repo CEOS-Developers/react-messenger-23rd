f89186ea079d5fa9cf1829ee72284117793f1e28
girimNam <rlfla@naver.com>
2026-04-05 23:07:01 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './pages/Chat'
-import ChatPage from './pages/Chat'
-
+import Chat from '@/pages/Chat'
+import ChatList from '@/pages/ChatList'
+import Friends from '@/pages/Friends'
+import MyProfile from '@/pages/MyProfile'
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="app">
-      <ChatPage />
+      <Chat />
     </div>
   )
 }
---
+++ src/routes/PublicRoutes.tsx
@@ -0,0 +1,41 @@
+import { type RouteObject, Navigate } from 'react-router-dom'
+
+import PublicLayout from '@/layout/PublicLayout'
+import ChatList from '@/pages/ChatList'
+import Chat from '@/pages/Chat'
+import MyProfile from '@/pages/MyProfile'
+import Friends from '@/pages/Friends'
+
+export const publicRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <PublicLayout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <Navigate
+            to="myprofile"
+            replace
+          />
+        ),
+      },
+      {
+        path: 'chatlist',
+        element: <ChatList />,
+      },
+      {
+        path: 'chat/:id',
+        element: <Chat />,
+      },
+      {
+        path: 'myprofile',
+        element: <MyProfile />,
+      },
+      {
+        path: 'friends',
+        element: <Friends />,
+      },
+    ],
+  },
+]
