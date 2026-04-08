c54d4744699dc4553befd5e04d088f9195dc2a42
a-00-a <sy.lee.2391@gmail.com>
2026-04-08 15:47:23 +0900

    Feat: 친구목록 페이지 레이아웃 구현

--- react-messenger-23rd/src/pages/friends/ui/FriendsPage.tsx
+++ react-messenger-23rd/src/pages/friends/ui/FriendsPage.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import BottomTabBar from '@/widgets/chat-list/ui/BottomTabBar';
 import FriendsHeader from '@/widgets/friends/ui/FriendsHeader';
 import FriendsList from '@/widgets/friends/ui/FriendsList';
@@ -5,14 +7,18 @@ import MyProfileSection from '@/widgets/friends/ui/MyProfileSection';
 import TagSection from '@/widgets/friends/ui/TagSection';
 
 const FriendsPage = () => {
+  const [isTagOpen, setIsTagOpen] = useState(true);
+  const [isFriendsOpen, setIsFriendsOpen] = useState(true);
+
   return (
     <main className="flex h-full flex-col bg-white">
-      <FriendsHeader />
-
-      <section className="flex-1 overflow-y-auto">
+      <section className="flex-1 overflow-y-auto scrollbar-hide">
+        <FriendsHeader />
         <MyProfileSection />
-        <TagSection />
-        <FriendsList />
+
+        <TagSection isOpen={isTagOpen} onToggle={() => setIsTagOpen((prev) => !prev)} />
+
+        <FriendsList isOpen={isFriendsOpen} onToggle={() => setIsFriendsOpen((prev) => !prev)} />
       </section>
 
       <BottomTabBar />
