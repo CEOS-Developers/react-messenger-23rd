1049d03d2f8eac00e064276371cc7447cd74f404
YJ <hghk0046@gmail.com>
2026-04-08 13:00:58 +0900

    fix: 이미지 절대경로 -> 상대경로

---
+++ messenger_app/src/utils/imageMaps.ts
@@ -0,0 +1,19 @@
+import profile_janchi from '../assets/profile_janchi.png';
+import profile_nosia from '../assets/profile_nosia.png';
+import profile_elena from '../assets/profile_elena.png';
+import profile_suyeon from '../assets/profile_suyeon.png';
+import profile_daram from '../assets/profile_me.png';
+import profile_igyu from '../assets/profile_igyu.png';
+import profile_hyon from '../assets/profile_hyon.png';
+import profile_me from '../assets/profile_me.png';
+
+export const imageMap: Record<string, string> = {
+    "profile_janchi": profile_janchi,
+    "profile_nosia": profile_nosia,
+    "profile_elena": profile_elena,
+    "profile_suyeon": profile_suyeon,
+    "profile_daram": profile_daram,
+    "profile_igyu": profile_igyu,
+    "profile_hyon": profile_hyon,
+    "profile_me": profile_me
+};
