5edefcf71cdf838eb02bf36e79fab07e2598475d
a-00-a <sy.lee.2391@gmail.com>
2026-04-08 15:29:33 +0900

    Fix: 채팅목록 내 프로필 아이콘 추가

--- react-messenger-23rd/src/widgets/friends/ui/MyProfileSection.tsx
+++ react-messenger-23rd/src/widgets/friends/ui/MyProfileSection.tsx
@@ -1,12 +1,18 @@
+import filesIcon from '@/shared/assets/icons/friends/folder-download.svg';
+import profileIcon from '@/shared/assets/icons/friends/user-02.svg';
+
 const MyProfileSection = () => {
   return (
-    <section className="flex items-center justify-between px-4 py-5">
+    <section className="flex items-center justify-between px-4 py-5 mb-5">
       <div className="flex items-center gap-3">
-        <div className="h-12 w-12 rounded-[10.7px] bg-main-light" />
+        <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-[10.7px] bg-main-light2">
+          <img src={profileIcon} alt="프로필" className="h-[39.27px] w-[39.27px] text-main-light" />
+        </div>
         <span className="Heading02SB">고다현</span>
       </div>
 
-      <button className="flex items-center gap-1 rounded-[24px] border px-4 py-2">
+      <button type="button" className="inline-flex items-center gap-1 rounded-[24px] border border-gray-40 px-3 py-2">
+        <img src={filesIcon} alt="프로필" className="h-[18px] w-[18px] text-gray-80" />
         <span className="Body03M text-gray-80">내 파일 보관함</span>
       </button>
     </section>
