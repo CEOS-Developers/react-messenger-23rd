020a982bb0476fe8720db604126326239d29ae54
YJ <hghk0046@gmail.com>
2026-04-09 15:05:12 +0900

    fix: 서치바 의존성 수정

--- messenger_app/pages/ChatListPage.tsx
+++ messenger_app/pages/ChatListPage.tsx
@@ -4,13 +4,16 @@ import { SearchBar } from '../src/components/utils/SearchBar';
 import { NavBar } from '../src/components/utils/Navbar';
 import { MainChatHeader } from '../src/components/chat/MainChatHeader';
 import { ChatList } from '../src/components/chat/ChatList';
+import { useState } from 'react';
 
 export const ChatListPage = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
     <div className="flex flex-col py-1.25">
       <MainChatHeader chatTitle="채팅"/>
 
-      <SearchBar/>
+      <SearchBar value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
 
       <ChatCategory/>
 
