ba5af00c56e11b65c397bd215bc28af78955054d
girimNam <rlfla@naver.com>
2026-04-15 22:30:34 +0900

    fix: 전체 친구 인원 연동

--- src/Profile/Member.tsx
+++ src/Profile/Member.tsx
@@ -1,14 +1,26 @@
 import RightArrow from '@assets/RightArrow.svg'
 import { useNavigate } from 'react-router-dom'
+import type { Profile } from '@/types/profile'
+import { useEffect, useState } from 'react'
 
 function Member() {
   const navigate = useNavigate()
+  const [members, setMembers] = useState<Profile[]>([])
+
+  useEffect(() => {
+    fetch('/data/member.json')
+      .then((res) => res.json())
+      .then((data) => setMembers(data.members))
+      .catch((err) => console.error('멤버 데이터를 불러오지 못했습니다.', err))
+  }, [])
 
   return (
     <div className="flex flex-row justify-between items-center h-15.5 border-b border-gray-20 px-4 py-5">
       <div className="flex flex-row gap-1.5">
         <p className="text-title2 antialiased">전체보기</p>
-        <p className="text-body1_r text-gray-70 antialiased">778</p>
+        <p className="text-body1_r text-gray-70 antialiased">
+          {members.length}
+        </p>
       </div>
 
       <button
