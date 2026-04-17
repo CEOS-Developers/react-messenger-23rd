244e3f931bc9949fa18f405369d29070c9fda0e7
girimNam <rlfla@naver.com>
2026-04-18 02:35:44 +0900

    refactor: 라우터 관련 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
-import Chat from '@/pages/Chat'
-import ChatList from '@/pages/ChatList'
-import Friends from '@/pages/Friends'
-import MyProfile from '@/pages/MyProfile'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import { MembersProvider } from '@/context/MembersContext'
+import { router } from '@/routes/router'
+
 function App() {
   return (
-    <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/myprofile"
-            element={<MyProfile />}
-          />
-          <Route
-            path="/friends"
-            element={<Friends />}
-          />
-          <Route
-            path="/chatlist"
-            element={<ChatList />}
-          />
-          <Route
-            path="/chat"
-            element={<Chat />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <MembersProvider>
+      <RouterProvider router={router} />
+    </MembersProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/router'
-import { MembersProvider } from './context/MembersContext'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MembersProvider>
-      <RouterProvider router={router} />
-    </MembersProvider>
+    <App />
   </StrictMode>
 )
