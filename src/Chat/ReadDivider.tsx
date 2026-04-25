babfd5febf9d505d9e0fb6ad821ad652f52c5f62
girimNam <rlfla@naver.com>
2026-04-25 12:14:55 +0900

    chore: 아이콘 추가 및 정렬 수정

--- src/Chat/ReadDivider.tsx
+++ src/Chat/ReadDivider.tsx
@@ -1,7 +1,10 @@
+import HereArrow from '@assets/icon-here.svg'
+
 function ReadDivider() {
   return (
     <div className="flex justify-center my-3">
-      <span className="bg-gray-60 text-gray-5 text-caption2_r antialiased px-3 py-1 rounded-full">
+      <span className="flex items-center gap-0.5 bg-gray-40 text-gray-95 text-caption2_r antialiased px-3 py-1 rounded-full">
+        <img src={HereArrow} className="w-4 h-4" />
         여기까지 읽었습니다.
       </span>
     </div>
